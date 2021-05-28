f265542cb9020d89971940c5fb1218c7ae0170a4
mariogruizdiaz <mariogruizdiaz@users.noreply.github.com>
2021-05-28 23:27:23 +0000

    deploy: 56e7e1b67b530c43600dd459133e206e14bb0df2

---
+++ precache-manifest.7aa22096cda973e535b9f02e9253cbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e89af3b64e0b2b0c91e57c00505bc1",
+    "url": "/adme-online/index.html"
+  },
+  {
+    "revision": "ce167bcda2e6a2dd8171",
+    "url": "/adme-online/static/css/2.b398de32.chunk.css"
+  },
+  {
+    "revision": "ce167bcda2e6a2dd8171",
+    "url": "/adme-online/static/js/2.06014296.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/adme-online/static/js/2.06014296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db147ea942709a1751d",
+    "url": "/adme-online/static/js/main.1b2ad34f.chunk.js"
+  },
+  {
+    "revision": "8c0175d146af90889ab4",
+    "url": "/adme-online/static/js/runtime-main.8cdfc3cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0c303c8eb805ad4cfbfbcad642770612.js"
+  "/adme-online/precache-manifest.7aa22096cda973e535b9f02e9253cbf3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adme-online/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
