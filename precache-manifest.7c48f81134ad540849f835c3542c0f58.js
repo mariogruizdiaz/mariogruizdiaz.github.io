5f7c5bbc23a3f1293efed06d0286a2be04cf83f3
mariogruizdiaz <mariogruizdiaz@users.noreply.github.com>
2021-05-28 16:39:38 +0000

    deploy: 5d8fd6b227afcc18e1a83c49c2e178388844e4c3

---
+++ precache-manifest.7c48f81134ad540849f835c3542c0f58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e22d05bc74af0f38d006dd76b21d91",
+    "url": "/adme-online/index.html"
+  },
+  {
+    "revision": "ce167bcda2e6a2dd8171",
+    "url": "/adme-online/static/css/2.b398de32.chunk.css"
+  },
+  {
+    "revision": "ce167bcda2e6a2dd8171",
+    "url": "/adme-online/static/js/2.06014296.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/adme-online/static/js/2.06014296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83519961f1cd4b60dfbe",
+    "url": "/adme-online/static/js/main.9ba0bfe8.chunk.js"
+  },
+  {
+    "revision": "8c0175d146af90889ab4",
+    "url": "/adme-online/static/js/runtime-main.8cdfc3cb.js"
+  }
+]);
