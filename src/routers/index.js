16cdf47aa1746a4b23d5ea951a9097270fdaabb1
mariogruizdiaz <mariogrd@gmail.com>
2021-05-30 16:27:23 -0300

    z

--- src/routers/index.js
+++ src/routers/index.js
@@ -54,7 +54,7 @@ export default class Routes extends React.PureComponent {
             <Route exact path="/faq" component={Faq} />
             <Route exact path="/404" component={NotFound} />
             <Route exact path="/comingSoon" component={ComingSoon} />
-            <Route exact path={`${process.env.PUBLIC_URL}/comingSoonBrands`} component={ComingSoonBrands} />
+            <Route path={`${process.env.PUBLIC_URL}/comingSoonBrands`} component={ComingSoonBrands} />
             <Route exact path={`${process.env.PUBLIC_URL}/comingSoonInvestors`} component={ComingSoonInvestors} />
             <Route exact path="/thankYou" component={ThankYou} />
             <Route exact path="/team" component={Team} />
