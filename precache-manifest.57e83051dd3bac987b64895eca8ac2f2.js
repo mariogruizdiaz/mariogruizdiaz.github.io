5b3f99d91063fa7a0a6e3e5531ac43379bf71a38
gastonrd7 <gastonrd7@users.noreply.github.com>
2024-03-12 22:08:15 +0000

    deploy: 6711a17116a8fddd4b40043a8d5c5212c3512090

---
+++ precache-manifest.57e83051dd3bac987b64895eca8ac2f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13dd4c57140f69325a2d866c0a7587d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3acb4f78c04337f4b71c",
+    "url": "/static/css/2.b398de32.chunk.css"
+  },
+  {
+    "revision": "3ce69d70cf76cf64f5ef",
+    "url": "/static/css/main.2bc55ecf.chunk.css"
+  },
+  {
+    "revision": "3acb4f78c04337f4b71c",
+    "url": "/static/js/2.d8916c8f.chunk.js"
+  },
+  {
+    "revision": "9f09ab559400a920c8d4ee5952fb6115",
+    "url": "/static/js/2.d8916c8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce69d70cf76cf64f5ef",
+    "url": "/static/js/main.c49d0789.chunk.js"
+  },
+  {
+    "revision": "e259b9568db1c937e913",
+    "url": "/static/js/runtime-main.5f4428d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0934bd7b203cb05bf9d385887f6facf2.js"
+  "/precache-manifest.57e83051dd3bac987b64895eca8ac2f2.js"
 );
 
 self.addEventListener('message', (event) => {
